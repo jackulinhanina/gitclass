1897c2cc75d9297f5df075732dd27d2d21b75f55
jackulinhanina <jackulinhanina22@gmail.com>
2024-03-04 13:49:34 +0530

    check my adding file

---
+++ src/main/java/org/caution/Thailand.java
@@ -0,0 +1,22 @@
+package org.caution;
+
+import org.testng.annotations.Test;
+
+public class Thailand {
+	@Test
+	public void tc89()
+	{
+		System.out.println("testcase89");
+	}
+	@Test
+	public void tc84()
+	{
+		System.out.println("test case84");
+	}
+	@Test
+	public void tc83()
+	{
+		System.out.println("test case83");
+	}
+
+}
